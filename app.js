8304751f6669ad3c98bc8bb3e1eff30f9aceb242
Nikica <novakovicnikica@gmail.com>
2016-12-24 19:21:21 +0000

    Configure passport

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-var express = require("express");
+var express = require("express"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    bodyParser = require("body-parser"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose"),
+    User = require("./models/user.js"),
+    expressSession = require("express-session"),
+    app = express();
 
-var app = express("");
+mongoose.connect("mongodb://localhost/auth_demo");
 
 app.set("view engine", "ejs");
+app.use(expressSession({
+    secret: "Nomi is grey",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app("/", function(req, res) {
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/", function(req, res) {
     res.render("home");
+});
+
+app.get("/secret", function(req, res) {
+    res.render("secret");
 });
 
 //start server with C9 IP and port
